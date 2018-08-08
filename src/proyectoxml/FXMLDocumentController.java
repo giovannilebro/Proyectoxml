12b00e4b9d4166a211150d07853b1891b202ad67
giovannilebro <36710480+giovannilebro@users.noreply.github.com>
2018-08-08 14:58:40 -0500

    cree interfaz agregar empleado con dos botones

--- src/proyectoxml/FXMLDocumentController.java
+++ src/proyectoxml/FXMLDocumentController.java
@@ -6,11 +6,15 @@
 package proyectoxml;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
+import modelo.Empleado;
 
 /**
  *
@@ -19,17 +23,54 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private Label ltitulo;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Label lnombre;
+    @FXML
+    private Label ldireccion;
+    @FXML
+    private Label ledad;
+    @FXML
+    private Label lcode;
+    @FXML
+    private Label ldepart;
+    @FXML
+    private TextField txtnombre;
+    @FXML
+    private TextField txtdireccion;
+    @FXML
+    private TextField txtedad;
+    @FXML
+    private TextField txtcode;
+    @FXML
+    private TextField txtdepart;
+  
+    LinkedList<Empleado>listEmpleado;
+ 
+    @FXML
+    private void AgregarEmpleadoLista(ActionEvent event) {
+       String nombre=txtnombre.getText();
+       String direccion=txtdireccion.getText();
+       String departamento=txtdepart.getText();
+       String codigo=txtcode.getText();
+       int edad=Integer.parseInt(txtedad.getText());
+       Empleado objEmpleado=new Empleado(codigo, departamento, nombre, direccion, edad);
+        listEmpleado.add(objEmpleado);
+        limpiarCampos();
+    
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+      listEmpleado=new LinkedList<>();
     }    
+
+    private void limpiarCampos() {
+        txtnombre.setText("");
+        txtdireccion.setText("");
+        txtedad.setText("");
+        txtcode.setText("");
+        txtdepart.setText("");
+    }
     
 }
